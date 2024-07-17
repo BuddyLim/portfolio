e17925128fd0fece72896bd68ab9a82c1e06bd78
Kuang Tar Lim <buddy.tlimk@gmail.com>
2024-07-17 17:04:15 +0800

    [Add] Precommit linting

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["end 0.1", "start 0.9"],
+    offset: ["end 0.1", "start 0.5"],
   });
   const opacity = useTransform(
     scrollYProgress,
